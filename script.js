237d3a266b2950ebe86057134b88d03df079180b
siah01 <josiah.wilkinson@gmail.com>
2025-06-06 19:49:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1818,7 +1818,7 @@ $(function () {
 */ 
 $("#financeButton").on('click', function () {
     $("#buttons").hide();
-    $("#leave").show();
+    $("#leavebutton").show();
     $("#finance").show().html('');
     $("#events").hide();
 
