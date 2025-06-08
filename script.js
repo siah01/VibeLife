9718b25b091fd40db7e97e16cecc9487c96d3168
siah01 <josiah.wilkinson@gmail.com>
2025-06-07 20:29:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ function comify(x) {
 }
 
  function update() {
+     console.log("update called", you)
     // Defensive checks for nested properties
     if (you['school']) {
         if (typeof you['school']['grade'] === "number") {
